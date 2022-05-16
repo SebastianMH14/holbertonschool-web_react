9e3b1cf005aa07bb26b5d7a7d6fa74da7d2c3ace
SebastianMH14 <sebasmh2002@hotmail.com>
2022-05-15 19:34:15 -0500

    fix requeriments

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -11,7 +11,7 @@ $(function(){
   $('body').append('<p>Holberton Dashboard</p>')
   $('body').append('<p>Dashboard data for the students</p>')
   $('body').append('<button>Click here to get started</button>')
-  $('body').append("<p id='count'></p>")
+  $('body').append($('<p>').attr('id', 'count'));
   $('body').append('<p>Copyright - Holberton School</p>')
   
   $('button').on('click', _.debounce(updateCounter, 500));
